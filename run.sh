7570bd1635eaf53bbc0e91823cab5c6a754918b1
osor1o <osorioneto97@gmail.com>
2020-11-20 12:38:33 -0300

    bugfix host and run script

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-
 printf "\n\033[0;44m---> Starting docker-compose.yml.\033[0m\n"
 sudo docker-compose up -d
 
@@ -6,4 +5,4 @@ printf "\n\033[0;44m---> Checking ansible syntax.\033[0m\n"
 ansible-playbook -i ansible/hosts -l linux_server -u main -k -b ansible/playbook.yml --syntax-check
 
 printf "\n\033[0;44m---> Starting ansible.\033[0m\n"
-ansible-playbook -i hosts -l linux_server -u main -k -b ansible/playbook.yml
+ansible-playbook -i ansible/hosts -l linux_server -u main -k -b ansible/playbook.yml
