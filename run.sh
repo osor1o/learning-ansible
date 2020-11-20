40f58ccf5465eca83751d39ba4303df4d11e0130
osor1o <osorioneto97@gmail.com>
2020-11-20 12:21:45 -0300

    create run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+
+printf "\n\033[0;44m---> Starting docker-compose.yml.\033[0m\n"
+sudo docker-compose up -d
+
+printf "\n\033[0;44m---> Checking ansible syntax.\033[0m\n"
+ansible-playbook -i ansible/hosts -l linux_server -u main -k -b ansible/playbook.yml --syntax-check
+
+printf "\n\033[0;44m---> Starting ansible.\033[0m\n"
+ansible-playbook -i hosts -l linux_server -u main -k -b ansible/playbook.yml
